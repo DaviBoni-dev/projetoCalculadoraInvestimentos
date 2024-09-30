693c88283fa065174b49ed4d71ef09e82572d9c7
Davi Boni <bonidavi07@gmail.com>
2024-09-30 11:27:33 -0300

    formulário completo

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 import { generateReturnsArray } from "./src/investmentGoals";
 
 const form = document.getElementById("investment-form");
+const clearButton = document.getElementById("clear");
 
 function renderProgression(evt) {
   evt.preventDefault();
+
+  if (document.querySelector(".error")) {
+    return;
+  }
+
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const additionalContribution = Number(
-    document.getElementById("addicional-contribution").value
+    document.getElementById("addicional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -28,4 +38,57 @@ function renderProgression(evt) {
   console.log(returnsArray);
 }
 
+function clearForm() {
+  for (const formElementClear of form) {
+    if (
+      formElementClear.tagName === "INPUT" &&
+      formElementClear.hasAttribute("name")
+    ) {
+      formElementClear.value = "";
+    }
+  }
+
+  const errorInputsContainers = document.querySelectorAll(".error");
+  for (const errorInputsContainer of errorInputsContainers) {
+    errorInputsContainer.classList.remove("error");
+    errorInputsContainer.parentElement.querySelector("p").remove();
+  }
+}
+
+function validateInput(evt) {
+  if (evt.target.value === "") {
+    return;
+  }
+
+  const { parentElement } = evt.target;
+  const grandParentElement = evt.target.parentElement.parentElement;
+  const inputValue = evt.target.value.replace(",", ".");
+
+  if (
+    (isNaN(inputValue) || Number(inputValue) <= 0) &&
+    !parentElement.classList.contains("error")
+  ) {
+    const errorTextElement = document.createElement("p");
+    errorTextElement.classList.add("text-red-500");
+    errorTextElement.innerText = `Insira um valor numérico e maior que 0`;
+
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(errorTextElement);
+  } else if (
+    parentElement.classList.contains("error") &&
+    !isNaN(inputValue) &&
+    Number(inputValue) > 0
+  ) {
+    parentElement.classList.remove("error");
+    grandParentElement.querySelector("p").remove();
+  }
+}
+
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
+}
+
 form.addEventListener("submit", renderProgression);
+clearButton.addEventListener("click", clearForm);
