7cb828eef9f9e6457bca478d714e1e4ae0e72671
Davi Boni <bonidavi07@gmail.com>
2024-09-30 23:53:54 -0300

    Processos relacionados aos gráficos foram comentados

--- main.js
+++ main.js
@@ -177,5 +177,5 @@ for (const formElement of form) {
   }
 }
 
-form.addEventListener("submit", renderProgression);
+// form.addEventListener("submit", renderProgression);
 clearButton.addEventListener("click", clearForm);
